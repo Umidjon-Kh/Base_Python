1b3b0a16e8244479ce91601846a8085530f6f6f3
Umidjon Khodjaev <iexistedonlyforyou@gmail.com>
2026-03-08 18:38:48 +0500

    Added inits

--- file_organizer/organizer/infrastructure/__init__.py
+++ file_organizer/organizer/infrastructure/__init__.py
@@ -1,5 +1,12 @@
 from .rules import JsonRuleRepository, InMemoryRuleRepository
+from .styles import JsonStyleRepository, InMemoryStyleRepository
 from .file_system import OSFileSystem
 
 
-__all__ = ['JsonRuleRepository', 'InMemoryRuleRepository', 'OSFileSystem']
+__all__ = [
+    'JsonRuleRepository',
+    'InMemoryRuleRepository',
+    'JsonStyleRepository',
+    'InMemoryStyleRepository',
+    'OSFileSystem',
+]
--- file_organizer/organizer/infrastructure/logging/__init__.py
+++
@@ -1,4 +0,0 @@
-from .in_memory_style_repo import InMemoryStyleRepository
-from .json_style_repo import JsonStyleRepository
-
-__all__ = ['InMemoryStyleRepository', 'JsonStyleRepository']
