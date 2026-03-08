603a52ec2270a76b6b8b7b2fb43427f2495b9267
Umidjon Khodjaev <iexistedonlyforyou@gmail.com>
2026-03-08 21:05:34 +0500

    added abstract class of logger

--- file_organizer/organizer/application/ports/__init__.py
+++ file_organizer/organizer/application/ports/__init__.py
@@ -1,5 +1,6 @@
 from .rule_repository import RuleRepository
 from .file_system import FileSystem
 from .style_repository import StyleRepository
+from .logger import Logger
 
-__all__ = ['RuleRepository', 'FileSystem', 'StyleRepository']
+__all__ = ['RuleRepository', 'FileSystem', 'StyleRepository', 'Logger']
---
+++ file_organizer/organizer/application/ports/logger.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Logger(ABC):
+    """
+    Abstract interface for logging.
+    Any concrete logger adapter must implement this.
+    """
+
+    @abstractmethod
+    def debug(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        """Log a debug message."""
+        pass
+
+    @abstractmethod
+    def info(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        """Log an info message."""
+        pass
+
+    @abstractmethod
+    def warning(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        """Log a warning message."""
+        pass
+
+    @abstractmethod
+    def error(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        """Log an error message."""
+        pass
+
+    @abstractmethod
+    def critical(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        """Log a critical message."""
+        pass
+
+    @abstractmethod
+    def exception(self, msg: str, *args: Any, **kwargs: Any) -> None:
+        """Log an exception with traceback."""
+        pass
