5b300c59173deb1a530f0969c3dfaad052ccb29d
Umidjon Khodjaev <iexistedonlyforyou@gmail.com>
2026-03-08 17:47:49 +0500

    Added style set like in rules

---
+++ file_organizer/organizer/domain/style_set.py
@@ -0,0 +1,31 @@
+from typing import Dict
+
+# Project modules
+from .styles import LevelStyle, DebugStyle, InfoStyle, WarningStyle, ErrorStyle, CriticalStyle
+from .exceptions import StyleNotFoundError
+
+
+class StyleSet:
+    __slots__ = ('_styles',)
+
+    def __init__(self, styles: Dict[str, LevelStyle]) -> None:
+        # Default styles for all levels (with empty config = defaults)
+        default_styles = {
+            'debug': DebugStyle({}),
+            'info': InfoStyle({}),
+            'warning': WarningStyle({}),
+            'error': ErrorStyle({}),
+            'critical': CriticalStyle({}),
+        }
+        # Merge: user styles override defaults
+        self._styles = {**default_styles, **{k.lower(): v for k, v in styles.items()}}
+
+    def get_style(self, level_name: str) -> LevelStyle:
+        """
+        Return style for the given level name (case-insensitive).
+        """
+        key = level_name.lower()
+        if key not in self._styles:
+            # This should never happen because we have defaults, but just in case
+            raise StyleNotFoundError(f'No style found for level: {level_name}')
+        return self._styles[key]
