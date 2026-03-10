f2d2b790aa3a89474c5a296389de89e1760062ff
Umidjon Khodjaev <iexistedonlyforyou@gmail.com>
2026-03-10 23:03:08 +0500

    Again changed ligic of inmemorystylesrepo

--- file_organizer/organizer/infrastructure/styles/in_memory_style_repo.py
+++ file_organizer/organizer/infrastructure/styles/in_memory_style_repo.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import Dict, Any, Optional
 
 # Project modules
@@ -25,11 +26,12 @@ class InMemoryStyleRepository(StyleRepository):
     Unknown level names will raise UnknownStyleType.
     """
 
-    __slots__ = ('styles_cfg', 'default_repo', 'combine')
+    __slots__ = ('styles_data', 'styles_file', 'default_repo', 'combine')
 
     def __init__(
         self,
-        styles_cfg: Dict[str, Dict[str, Any]],
+        styles_data: Optional[Dict[str, Dict[str, Any]]] = None,
+        styles_repo: Optional[StyleRepository] = None,
         default_repo: Optional[StyleRepository] = None,
         combine: bool = False,
     ) -> None:
@@ -39,26 +41,56 @@ class InMemoryStyleRepository(StyleRepository):
             default_repo: Repository to load default styles from (if combine=True).
             combine: If True, merge default styles with provided styles cfg
         """
-        self.styles_cfg = styles_cfg
+        self.styles_data = styles_data
+        self.styles_file = styles_repo
         self.default_repo = default_repo
         self.combine = combine
 
     def load_styles(self) -> StyleSet:
+        styles_data = {}
         if self.combine and self.default_repo:
-            default_set = self.default_repo.load_styles()
-            # Build user styles
-            user_styles = self._build_styles_from_dict(self.styles_cfg)
-            combined_styles = {**default_set.styles, **user_styles}
-            return StyleSet(combined_styles)
+            styles_data = self.default_repo.load_styles().styles
+            # If user styles-file is not None
+            if self.styles_file is not None:
+                # Deep merging user styles and default styles
+                user_styles = self.styles_file.load_styles().styles
+                styles_data = self._deep_merge(styles_data, user_styles)
+            if self.styles_data is not None:
+                # Deep merging styles data and styles_data
+                styles_data = self._deep_merge(styles_data, self.styles_data)
+            # Building styles from merged styles_data
+            styles = self._build_styles_from_dict(styles_data)
+            # Returning StyleSet
+            return StyleSet(styles)
         else:
-            user_styles = self._build_styles_from_dict(self.styles_cfg)
-            return StyleSet(user_styles)
+            if self.styles_file is not None:
+                styles_data = self.styles_file.load_styles().styles
+                if self.styles_data is not None:
+                    styles_data = self._deep_merge(styles_data, self.styles_data)
+            # Building styles from styles_data
+            styles = self._build_styles_from_dict(styles_data)
+            # Returning StyleSet
+            return StyleSet(styles)
 
-    def _build_styles_from_dict(self, styles_cfg: Dict[str, Any]) -> Dict[str, LevelStyle]:
-        if not isinstance(styles_cfg, dict):
+    def _deep_merge(self, non_priority_dict: Dict, priority_dict: Dict) -> Dict:
+        """Returns recursively deep merged dict"""
+        base = deepcopy(non_priority_dict)
+        # Iterating dict items if item value is dict calling to recursively copy deep_merge
+        for key, value in priority_dict.items():
+            # if both values are dict type
+            if key in base and isinstance(base[key], dict) and isinstance(value, dict):
+                base[key] = self._deep_merge(base[key], value)
+            else:
+                # Otherwise, overwrite the value in base dict
+                base[key] = value
+
+        return base
+
+    def _build_styles_from_dict(self, styles_data: Dict[str, Any]) -> Dict[str, LevelStyle]:
+        if not isinstance(styles_data, dict):
             raise StyleFormatError('\'styles\' must be a dict')
         styles: Dict[str, LevelStyle] = {}
-        for level, config in styles_cfg.items():
+        for level, config in styles_data.items():
             style = self._create_style(level, config)
             styles[level] = style
         return styles
