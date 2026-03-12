c54b85fb544ebbe08042e2e7507e6caa494b36ec
Umidjon Khodjaev <iexistedonlyforyou@gmail.com>
2026-03-13 02:28:21 +0500

    just changing version to publish fixed version to pypi not necessary things

--- klart/setup.py
+++ klart/setup.py
@@ -5,7 +5,7 @@ setup(
     # Unique name of file organizer from svedish klat
     name='klart',
     # Version of package
-    version='1.0.2',
+    version='1.0.3',
     # Simple mini description of file organizer
     description='Organize files into folders based on configurable rules',
     long_description=open('README.md').read(),
